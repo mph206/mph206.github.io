3cd02b8ead30c178cbf9c6784377161672a07a23
Martin Harrison <mpharrison206@gmail.com>
2020-07-26 10:30:58 +0100

    Move projects to own data files

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+import { projects } from './projects.js'
+
+// Render projects
+const projectsMap = (element) => {
+  document.querySelector(element).innerHTML = projects.map((project, index) => {
+    return `<article id="project-${index}" class="hidden">
+        <img src=${project.img} alt=${project.name}>
+          <span class="skills">
+            ${project.icons.map(icon => `<i class="${icon} float-icons"></i>`)}
+          </span>
+          <a href=${project.pageLink} target='_blank'><i class="fas fa-rocket float-icons"></i></a>
+          <a href=${project.gitLink} target='_blank'><i class="fas fa-code float-icons"></i></a>
+        <div class="project-text">  
+          <h3>${project.name}</h3>
+          <p>${project.description}</p>
+        </div>
+        </article>`
+  }).join('');
+} 
+projectsMap('.grid-container')
+
 // Make items appears as they scroll into view
 const animateOnScroll = () => {
   let elements;
@@ -65,3 +86,4 @@ modal.addEventListener('click', () => {
     hideModal();
   }
 });
+
