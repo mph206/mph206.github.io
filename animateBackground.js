30ce5f8d0b7bda45f831325a7d9cc1e9089a82f6
Martin Harrison <mpharrison206@gmail.com>
2020-07-28 21:06:34 +0100

    adjust background scroll for mobile

--- animateBackground.js
+++ animateBackground.js
@@ -16,8 +16,9 @@ export const animateBackground = () => {
   const checkPosition = () => {
     for (let i = 0; i < elements.length; i++) {
       let positionFromTop = elements[i].getBoundingClientRect().top;
+      // console.log(positionFromTop - windowHeight)
 
-      if (positionFromTop - windowHeight <= 0) {
+      if (positionFromTop - windowHeight < -2) {
         containers[0].classList.add('background-left');
         containers[1].classList.add('background-left');
 
