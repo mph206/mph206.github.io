375bbd6933d7eb20b00942b53ac07032a44cce55
Martin Harrison <mpharrison206@gmail.com>
2020-07-31 16:12:15 +0100

    add omdb project

--- projects.js
+++ projects.js
@@ -12,7 +12,7 @@ export const projects = [
         description: "An app build in React leveraging Spotify's API and a lyrics API to allow users to browse their favourite artists and find songs by them.",
         img: "./images/music-app.png",
         skills: "React | REST APIs | SCSS",
-        pageLink: '', // Hide private key and host
+        pageLink: '/music-app',
         gitLink: 'https://github.com/mph206/music-app',
     },{
         name: 'Keyboard Invaders',
@@ -21,7 +21,16 @@ export const projects = [
         skills: "JavaScript | HTML | SCSS",
         pageLink: './Keyboard-invaders', 
         gitLink: 'https://github.com/mph206/Keyboard-invaders',
-    },{
+    },
+    {
+        name: "OMDb Search",
+        description: "An app build in React leveraging as part of a one day tech-test, using OMDbs API to let users find search and view movie information.",
+        img: "./images/omdb-search.png",
+        skills: "React | REST APIs | SCSS",
+        pageLink: '/OMDb-search',
+        gitLink: 'https://github.com/mph206/OMDb-search',
+    },
+    {
         name: "Morse Code Translator",
         description: 'A site that converts Morse-English or English-Morse. Built in a few hours as part of a simulated tech test during week 4 of the _nology course, with some polish added later.',
         img: "./images/morse.png",
